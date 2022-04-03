c811522d0cdb2989e0196ae71fd7e6214411507d
LeaF-wut <65544388+LeaF-wut@users.noreply.github.com>
2022-04-03 17:13:11 +0200

    Delete gen.py

--- r/gen.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def get_random_string(length):
-    letters = ['I','l']
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    print(length, result_str)
-
-get_random_string(128)
